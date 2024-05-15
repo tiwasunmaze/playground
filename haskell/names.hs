501a81992fe8ec1cd05fe61839369f821e0552b0
tiwasunmaze <suman.tiwari@outlook.com>
2024-05-15 12:43:45 +0530

    chapter 9

--- haskell/names.hs
+++ haskell/names.hs
@@ -18,3 +18,27 @@ flipArgs f arg1 arg2 = f arg2 arg1
 div2 = (/) 2
 flipDiv2 = flip (/) 2
 
+
+subseq start end xs =
+    take (end - start) $ drop start xs 
+
+takeMy :: Int -> [p2] -> [p2]
+takeMy _ [] = []
+takeMy some (x:xs) = if some > 0 
+                        then x: takeMy (some-1) xs
+                        else []
+
+myMap :: (a -> a) -> [a] -> [a]
+myMap _ [] = []
+myMap f (x:xs) = f x:map f xs
+
+fg :: (a -> Bool) -> [a] -> [a]
+fg _ [] = []
+fg f (x:xs) = 
+    if f x
+    then x
+    else fg f xs []
+
+
+
+     
